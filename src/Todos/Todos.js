e0a50ee45a2e3cae4af8cab376bd06a7997d0f6e
Tianyu Ren <terrinaerin@163.com>
2021-07-26 16:16:49 +0800

    feat: add input box and submit button

--- src/Todos/Todos.js
+++ src/Todos/Todos.js
@@ -3,11 +3,22 @@ import React from 'react';
 export default function Todos() {
     const userName = 'Jing';
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
         <div>
-            <h1>TODO LIST</h1>
-            <h2>Hi {userName}, what’s your focus for today?</h2>
-
+            <section>
+              <h1>TODO LIST</h1>
+              <h2>Hi {userName}, what’s your focus for today?</h2>
+            </section>
+            <section>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder='Input whatever you like...'/>
+                    <button type='submit'>Add Task</button>
+                </form>
+            </section>
         </div>
     )
 }
